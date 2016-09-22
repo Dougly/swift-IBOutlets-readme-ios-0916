01f66377a92bdf79144a9e7a183aac66be2bac67
Dougly <dgalante11@gmail.com>
2016-09-22 12:44:00 -0400

    all buttons change label

--- ViewController.swift
+++ ViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textDisplay: UILabel!
 
+    @IBAction func helloWorld(_ sender: AnyObject) {
+        textDisplay.text = "Hello, world!"
+        print ("pressed the hello world button")
+    }
+    
+    
+    @IBAction func goodbyeWorld(_ sender: AnyObject) {
+        print("pressed the goodbye world button")
+        textDisplay.text = "Goodbye, world!"
+    }
+    
+    
+    @IBAction func unicorn(_ sender: AnyObject) {
+        print("pressed the unicorn! button")
+        textDisplay.text = "🦄"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz"
+        
     }
 
     override func didReceiveMemoryWarning() {
